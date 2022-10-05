9f440ab1baba813f1db891e82a2d9f9756d1ae8c
sri-rishi <sririshi2612@gmail.com>
2022-10-05 23:47:57 +0530

    feat: added image

--- src/index.js
+++ src/index.js
@@ -1,8 +1,10 @@
 import App from "./App";
+import WebpackLogo from "./webpack.svg";
 
 const root = document.getElementById("root");
 
 root.innerHTML = `
+<div class='logo'><img src=${WebpackLogo} alt='logo' /></div>
 <h1>Webpack Starter</h1>
 ${App({name: "Rishi"})}
 `;
