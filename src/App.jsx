fa0ffc176d9d6d3e5c07b616d8e2b7418fe4b65a
sri-rishi <sririshi2612@gmail.com>
2022-10-11 01:06:05 +0530

    feat: added lazy loading and toggle button

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,24 @@
 import "./App.css";
 import _ from "lodash";
 import WebpackLogo from "./webpack.svg";
+import { useState } from "react";
+const Info = React.lazy(() => import("./Info"))
 
 const App = ({name}) => {
+    const [showInfo, setShowInfo] = useState(false);
+
+    const handleShowInfo = () => setShowInfo(!showInfo);
+
     return (
         <div className="main">
             <div className="logo-box">
                 <img src={WebpackLogo} loading="lazy" alt="logo"/>
             </div>
             <h1>{_.join(["Welcome", name], " ")}</h1>
+            <button onClick={() => handleShowInfo()}>{showInfo ? "Hide": "Show"} Info</button>
+            {
+                showInfo && <Info />
+            }
         </div>
     )
 }
