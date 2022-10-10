e037c812263423c8b6e4b552b34630ae0eaa4c0f
sri-rishi <sririshi2612@gmail.com>
2022-10-11 01:05:20 +0530

    feat: added info

---
+++ src/Info.jsx
@@ -0,0 +1,11 @@
+export const Info = () => {
+    return (
+        <div>
+            <p>Hey there it's a webpack starter app</p>
+            <div>
+                <p>For installation and source code:</p>
+                <a href="#">webpack-starter</a>
+            </div>
+        </div>
+    )
+}
